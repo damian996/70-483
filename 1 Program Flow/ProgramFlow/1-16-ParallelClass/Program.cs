2997ccc4bf97afaf0503c413042ce804bef80197
Damian Kaminski <damian.kaminski@DK-LAPTOP.uk.pinewood.net>
2016-01-29 16:23:01 +0000

    Progress

---
+++ 1 Program Flow/ProgramFlow/1-16-ParallelClass/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_16_ParallelClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- 1 Program Flow/ProgramFlow/1-8-Task/Program.cs
+++ 1 Program Flow/ProgramFlow/1-8-Task/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace _1_8_Task
@@ -133,8 +134,85 @@ namespace _1_8_Task
                     Console.WriteLine(i);
             });
 
-            finalTask.Wait();
+            finalTask.Wait(); //FINAL TASK WILL ONLY RUN ONCE THE PARENT TASK HAS FINISHED
 
+            Console.WriteLine("  ");
+            Console.WriteLine("1-14");
+            Console.WriteLine("  ");
+
+            //1-14 In this case all tasks are exeuted simultaneously 
+
+            Task<int>[] tasks = new Task<int>[3];
+
+            tasks[0] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Task 1");
+                return 1;
+            });
+
+            tasks[1] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Task 2");
+                return 2;
+            });
+
+            tasks[2] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Task 3");
+                return 3;
+            });
+
+            Task.WaitAll(tasks); //will wait until all tasks have finished.
+
+            //Task.WhenAll(tasks); use this to specify continuation method
+
+            //Task.WaitAny(tasks); use this to wait until any of the tasks has finished - example below.
+
+            Console.WriteLine("  ");
+            Console.WriteLine("1-15");
+            Console.WriteLine("  ");
+
+            //1-15 In this example we process a completed Task as soon as it finished. 
+            //Finished tasks are processed in the while loop and then removed from the array.
+            //By keeping track of which tasks are finished we don't neet wait until all tasks have completed.
+
+            tasks = new Task<int>[3];
+
+            tasks[0] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Task 1");
+                return 1;
+            });
+
+            tasks[1] = Task.Run(() =>
+            {
+                Thread.Sleep(3000);
+                Console.WriteLine("Task 2");
+                return 2;
+            });
+
+            tasks[2] = Task.Run(() =>
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("Task 3");
+                return 3;
+            });
+
+            while(tasks.Length >0)
+            {
+                int i = Task.WaitAny(tasks);
+                Task<int> completedTask = tasks[i];
+                Console.WriteLine(completedTask.Result);
+
+                var temp = tasks.ToList();
+                temp.RemoveAt(i);
+                tasks = temp.ToArray();
+                
+            }
 
             Console.ReadKey();
         }  
